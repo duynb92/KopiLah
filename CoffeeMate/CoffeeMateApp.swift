1c755050027b28609b3f1a95b6857b1642dd785f
Duy <duynb92@gmail.com>
2026-01-05 23:26:16 +0800

    Initial Commit

---
+++ CoffeeMate/CoffeeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMateApp.swift
+//  CoffeeMate
+//
+//  Created by Duy on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
